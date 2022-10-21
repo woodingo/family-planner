3fbe03fdbd8d9761c0bd0c885af5355c18259518
andrew.yudin <andrew.yudin@nordclan.com>
2022-10-21 17:50:48 +0400

    auth

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { NavBar, Space, Toast } from 'antd-mobile';
+import { Button, NavBar, Space, Toast } from 'antd-mobile';
+import { createRoot } from 'react-dom/client';
+import '../firebase';
+import { doAuth } from './api/auth';
 
 const App = () => (
   <div>
     <NavBar>Hellooo</NavBar>
+    <Button onClick={doAuth}>do auth</Button>
   </div>
 );
 
-import { createRoot } from 'react-dom/client';
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(<App />);
